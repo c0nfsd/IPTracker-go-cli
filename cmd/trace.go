8ca41fc999a3600c0de8b70ba8c74e31184e0fd7
c0nfsd <pathakmilindd@gmail.com>
2022-08-17 20:27:33 +0530

    Ip struct

--- cmd/trace.go
+++ cmd/trace.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -24,5 +26,39 @@ var traceCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(traceCmd)
+
+}
+
+type Ip struct {
+	IP       string `json::"ip"`
+	City     string `json::"city"`
+	Region   string `json::"region"`
+	Country  string `json::"country"`
+	Loc      string `json::"loc"`
+	TimeZone string `json::"timezone"`
+	Postal   string `json::"postal"`
+}
+
+func showData() {
+	url := "http://ipinfo.io/1.1.1.1/geo"
+	responseByte := getData(url)
+
+}
+
+func getData(url string) []byte {
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println("Unable to read the response")
+	}
+
+	responseByte, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		fmt.Println("Unable to read the response")
+	}
+
+	return responseByte
 
 }
